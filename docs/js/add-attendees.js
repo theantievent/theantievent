ca35c56e65517de50e9763473da98ec5a910df2e
Naiara Abaroa <Naiara@Iparretxes-MacBook-Pro.local>
2025-06-22 22:30:41 +0200

    Update to 2025

--- docs/js/add-attendees.js
+++ docs/js/add-attendees.js
@@ -1,4 +1,4 @@
-const attendeesDiv = document.querySelector('.attenders');
+const historyDiv = document.querySelector('.js-attenders-historical');
 const createElement = element => document.createElement(element);
 
 const getAttendeesList = async () => {
@@ -6,49 +6,86 @@ const getAttendeesList = async () => {
   return response.json();
 };
 
+const getHistoricalAttendees = async () => {
+  const response = await fetch('./js/attendees-list-historical.json');
+  return response.json();
+};
+
+const renderAttendee = attendee => {
+  const item = createElement('div');
+  item.className = 'item';
+
+  const chItem = createElement('div');
+  chItem.className = 'ch-item';
+
+  const chInfo = createElement('div');
+  chInfo.className = 'ch-info';
+
+  const chInfoFront = createElement('div');
+  chInfoFront.className = 'ch-info-front';
+  const image = createElement('img');
+  image.src = attendee.avatar;
+
+  chInfoFront.appendChild(image);
+  chInfo.appendChild(chInfoFront);
+  chItem.appendChild(chInfo);
+  item.appendChild(chItem);
+
+  const chInfoBack = createElement('div');
+  chInfoBack.className = 'ch-info-back';
+
+  const name = createElement('h3');
+  name.innerText = attendee.name;
+
+  const description = createElement('p');
+  const link = createElement('a');
+  link.target = '_blank';
+  link.rel = 'noopener noreferrer';
+  link.text = attendee.nickname;
+  link.href = attendee.link;
+
+  description.appendChild(link);
+  chInfoBack.appendChild(name);
+  chInfoBack.appendChild(description);
+
+  chInfo.appendChild(chInfoBack);
+
+  return item;
+};
+
+const renderHistoricalAttendee = attendee => {
+  const item = createElement('div');
+  item.className = 'item';
+
+  const chItem = createElement('div');
+  chItem.className = 'ch-item';
+
+  const chInfo = createElement('div');
+  chInfo.className = 'ch-info';
+
+  const chInfoFront = createElement('div');
+  chInfoFront.className = 'ch-info-front';
+  const image = createElement('img');
+  image.src = attendee.avatar;
+
+  chInfoFront.appendChild(image);
+  chInfo.appendChild(chInfoFront);
+  chItem.appendChild(chInfo);
+  item.appendChild(chItem);
+
+  return item;
+};
+
 (async () => {
-  const antiAttendees = await getAttendeesList();
+  // const antiAttendees = await getAttendeesList();
+  // antiAttendees.forEach(attendee => {
+  //   const el = renderAttendee(attendee);
+  //   attendeesDiv.insertBefore(el, attendeesDiv.firstChild);
+  // });
 
-  antiAttendees.forEach(attendee => {
-    const item = createElement('div');
-    item.className = 'item';
-
-    const chItem = createElement('div');
-    chItem.className = 'ch-item';
-
-    const chInfo = createElement('div');
-    chInfo.className = 'ch-info';
-
-    const chInfoFront = createElement('div');
-    chInfoFront.className = 'ch-info-front';
-    const image = createElement('img');
-    image.src = attendee.avatar;
-
-    chInfoFront.appendChild(image);
-    chInfo.appendChild(chInfoFront);
-    chItem.appendChild(chInfo);
-    item.appendChild(chItem);
-
-    const chInfoBack = createElement('div');
-    chInfoBack.className = 'ch-info-back';
-
-    const name = createElement('h3');
-    name.innerText = attendee.name;
-
-    const description = createElement('p');
-
-    const link = createElement('a');
-    link.target = '_blank';
-    link.rel = 'noopener noreferrer';
-    link.text = attendee.nickname;
-    link.href = attendee.link;
-
-    description.appendChild(link);
-    chInfoBack.appendChild(name);
-    chInfoBack.appendChild(description);
-
-    chInfo.appendChild(chInfoBack);
-
-    attendeesDiv.insertBefore(item, attendeesDiv.firstChild);
+  const historicalAttendees = await getHistoricalAttendees();
+  historicalAttendees.forEach(attendee => {
+    const el = renderHistoricalAttendee(attendee);
+    historyDiv.insertBefore(el, historyDiv.firstChild);
   });
 })();
