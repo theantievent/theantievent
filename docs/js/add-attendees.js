049b30494dace08a7a6bbd51668d4781c8196aad
Naiara Abaroa <naiaraabaroa@gmail.com>
2025-08-31 21:24:31 +0200

    Update attendees

--- docs/js/add-attendees.js
+++ docs/js/add-attendees.js
@@ -1,59 +1,14 @@
+const attendeesDiv = document.querySelector('.js-attenders');
 const historyDiv = document.querySelector('.js-attenders-historical');
-const createElement = element => document.createElement(element);
+const createElement = el => document.createElement(el);
 
-const getAttendeesList = async () => {
-  const response = await fetch('./js/attendees-list.json');
+const fetchJSON = async path => {
+  const response = await fetch(path);
+  if (!response.ok) throw new Error(`Error cargando ${path}`);
   return response.json();
 };
 
-const getHistoricalAttendees = async () => {
-  const response = await fetch('./js/attendees-list-historical.json');
-  return response.json();
-};
-
-const renderAttendee = attendee => {
-  const item = createElement('div');
-  item.className = 'item';
-
-  const chItem = createElement('div');
-  chItem.className = 'ch-item';
-
-  const chInfo = createElement('div');
-  chInfo.className = 'ch-info';
-
-  const chInfoFront = createElement('div');
-  chInfoFront.className = 'ch-info-front ';
-  const image = createElement('img');
-  image.src = attendee.avatar;
-
-  chInfoFront.appendChild(image);
-  chInfo.appendChild(chInfoFront);
-  chItem.appendChild(chInfo);
-  item.appendChild(chItem);
-
-  const chInfoBack = createElement('div');
-  chInfoBack.className = 'ch-info-back';
-
-  const name = createElement('h3');
-  name.innerText = attendee.name;
-
-  const description = createElement('p');
-  const link = createElement('a');
-  link.target = '_blank';
-  link.rel = 'noopener noreferrer';
-  link.text = attendee.nickname;
-  link.href = attendee.link;
-
-  description.appendChild(link);
-  chInfoBack.appendChild(name);
-  chInfoBack.appendChild(description);
-
-  chInfo.appendChild(chInfoBack);
-
-  return item;
-};
-
-const renderHistoricalAttendee = attendee => {
+const renderAttendee = (attendee, isHistorical = false) => {
   const item = createElement('div');
   item.className = 'item';
 
@@ -67,10 +22,32 @@ const renderHistoricalAttendee = attendee => {
   chInfoFront.className = 'ch-info-front';
   const image = createElement('img');
   image.src = attendee.avatar;
-  image.className = 'nk-filter--grayscale';
+  if (isHistorical) image.className = 'nk-filter--grayscale';
 
   chInfoFront.appendChild(image);
   chInfo.appendChild(chInfoFront);
+
+  if (!isHistorical) {
+    const chInfoBack = createElement('div');
+    chInfoBack.className = 'ch-info-back';
+
+    const name = createElement('h3');
+    name.innerText = attendee.name;
+
+    const description = createElement('p');
+    const link = createElement('a');
+    link.target = '_blank';
+    link.rel = 'noopener noreferrer';
+    link.text = attendee.nickname;
+    link.href = attendee.link;
+
+    description.appendChild(link);
+    chInfoBack.appendChild(name);
+    chInfoBack.appendChild(description);
+
+    chInfo.appendChild(chInfoBack);
+  }
+
   chItem.appendChild(chInfo);
   item.appendChild(chItem);
 
@@ -78,15 +55,19 @@ const renderHistoricalAttendee = attendee => {
 };
 
 (async () => {
-  // const antiAttendees = await getAttendeesList();
-  // antiAttendees.forEach(attendee => {
-  //   const el = renderAttendee(attendee);
-  //   attendeesDiv.insertBefore(el, attendeesDiv.firstChild);
-  // });
+  try {
+    const attendees = await fetchJSON('./js/attendees-list.json');
+    attendees.forEach(attendee => {
+      const el = renderAttendee(attendee);
+      attendeesDiv.appendChild(el);
+    });
 
-  const historicalAttendees = await getHistoricalAttendees();
-  historicalAttendees.forEach(attendee => {
-    const el = renderHistoricalAttendee(attendee);
-    historyDiv.insertBefore(el, historyDiv.firstChild);
-  });
+    const historicalAttendees = await fetchJSON('./js/attendees-list-historical.json');
+    historicalAttendees.forEach(attendee => {
+      const el = renderAttendee(attendee, true);
+      historyDiv.appendChild(el);
+    });
+  } catch (e) {
+    console.error('Error cargando asistentes:', e);
+  }
 })();
